d339449375fb506eec3fe0c6fbcf0f0e1ed5b4cf
unknown <vegayes@naver.com>
2023-07-21 15:49:30 +0900

    bingo

--- src/edu/kh/array2/ex/Array2Example.java
+++ src/edu/kh/array2/ex/Array2Example.java
@@ -7,8 +7,7 @@ public class Array2Example {
 	/*
 	 * 2차원 배열
 	 * 
-	 * => 자료형이 같은 1차원 배열을 묶음으로 다루는 것 
-	 * => 행, 열 개념 추가 =>
+	 * => 자료형이 같은 1차원 배열을 묶음으로 다루는 것 => 행, 열 개념 추가 =>
 	 * 
 	 * 
 	 * 
@@ -73,70 +72,140 @@ public class Array2Example {
 
 	}
 
-	
 	public void ex2() {
-		
+
 		// 2차원 배열 선언과 동시에 초기화
 		// 3행 3열짜리 정수형 2차원 배열 선언과 동시에
 		// 1 ~ 9 까지 초기화
-		int[][] arr = {{1,2,3},{4,5,6},{7,8,9}};
-		
+		int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
 		// 행 별로 합 출력
-		
-		for ( int row = 0; row < arr.length; row ++) { // 행 반복
-			
-			int sum =0;
-			
+
+		for (int row = 0; row < arr.length; row++) { // 행 반복
+
+			int sum = 0;
+
 			for (int col = 0; col < arr[row].length; col++) { // 열 반복
-				
+
 				sum += arr[row][col];
-				//         0    0   --> 1
-				//         0    1   --> 2
-				//         0    2   --> 3
-	// -----------------------------------------------
-				//         1    0   --> 4
-				//         1    1   --> 5
-				//         1    2   --> 6
-	// -----------------------------------------------
-				//         2    0   --> 7
-				//         2    1   --> 8
-				//         2    2   --> 9
+				// 0 0 --> 1
+				// 0 1 --> 2
+				// 0 2 --> 3
+				// -----------------------------------------------
+				// 1 0 --> 4
+				// 1 1 --> 5
+				// 1 2 --> 6
+				// -----------------------------------------------
+				// 2 0 --> 7
+				// 2 1 --> 8
+				// 2 2 --> 9
 			}
-			
-			System.out.printf("%d행의 합 : %d\n",row, sum);
-			
+
+			System.out.printf("%d행의 합 : %d\n", row, sum);
+
 		}
-			
-		int sum2 = 0;
+
+		// 열 별로 합 출력 및 전체 출력
+		int total = 0;
 
 		System.out.println("-".repeat(50));
+
+		for (int a = 0; a < arr[0].length; a++) { // 열 반복
+
+			int sum = 0;
+
+			for (int b = 0; b < arr.length; b++) { //행 반복
+				sum += arr[a][b];
+				
+				// 0 0 --> 1
+				// 1 0 --> 4
+				// 2 0 --> 7
+				// -----------------------------------------------
+				// 0 1 --> 2
+				// 1 1 --> 5
+				// 2 1 --> 8
+				// -----------------------------------------------
+				// 0 2 --> 3
+				// 1 2 --> 6
+				// 2 2 --> 9
+				
+			}
+			System.out.printf("%d열의 합 : %d\n", a, sum);
+			
+			total += sum;
+		}
+
+		/* 전체 합 출력 다른 방법
+		int sum = 0; 
 		
-		for ( int a = 0; a < arr.length; a++) {
+		for(int row =0; row< arr.length; row++){
+		 	for( int col =0; col < arr[row].length; col++){
+		 		
+		 			sum+= arr[row][col];
+		 	}
+		 ]
+		*/
+		
+		System.out.println("전체 합 :" + total);
+
+	}
+	
+	
+	public void ex3() {
+		// 가변 배열 
+		// -> 2차원 배열 생성 시 마지막 배열 차수(열)를 지정하지 않고
+		//  나중에 서로 크기가 다른 1차원 배열을 생성하여 참조하는 배열
+		
+		char [][] arr = new char[4][];
+						// char 2차원 배열 생성 시 행 부분만 생성 
+		
+		arr[0] = new char[3] ;  // 0행 인덱스에 3열 1차원 배열 생성하여 주소값 저장
+		arr[1] = new char[4] ;  // 1행 인덱스에 4열 1차원 배열 생성하여 주소값 저장 
+		arr[2] = new char[5] ;  // 2행 인덱스에 5열 1차원 배열 생성하여 주소값 저장 
+		arr[3] = new char[2] ;  // 3행 인덱스에 2열 1차원 배열 생성하여 주소값 저장 
+		
+		char ch ='a';
+		
+		for ( int row = 0; row < arr.length; row++) {
+			for (int col = 0; col < arr[row].length; col++) { // 제일 긴 인덱스 값을 기준 잡으면 오류남. (해당행의 길이랑 맞지 않을 수 있기 때문)
+				
+				arr[row][col] = ch++;
+				
+			}
+
+		}
+		System.out.println(Arrays.deepToString(arr));
+	
+	}
+	
+	
+	
+	public void ex4() { // 빙고 만들기! 
+		// 5X 5 ( 1~25 ) random으로 넣기 
+		// 1 ~25의 숫자를 입력하세요 ( 종료 : 0) 
+		// 숫자 입력하면 0으로 변하게 하기! 
+		
+		// 배열에 넣기 ! ===>
+		// 1) 2차원 배열을 이용해서 25까지 랜덤한 값을 => 5로 나누어서 대입하기
+		// 2) 
+		
+		int[][] bingo;
+		bingo = new int[5][5];
+		
+		
+		int random = (int) (Math.random()*25 +1);
+		
+		for ( int col = 0; col <bingo.length; col++) {
 			
-			int sum =0;
-			
-			for( int b = 0; b< arr[a].length; b++) {
-				sum+= arr[b][a];
+			for ( int row = 0; row < bingo[col].length; row++) {
+				
+				
 			}
 			
-			System.out.printf("%d열의 합 : %d\n",a, sum);
-			sum2 += sum;
+			
 		}
 		
-		System.out.println("전체 합 :"+sum2);
-	
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
+		
 		
 		
 		
@@ -149,9 +218,5 @@ public class Array2Example {
 	
 	
 	
-	
-	
-	
-	
-	
+
 }
--- src/edu/kh/array2/ex/Array2Run.java
+++ src/edu/kh/array2/ex/Array2Run.java
@@ -8,7 +8,8 @@ public class Array2Run {
 		
 	
 //		ae2.ex1();
-		ae2.ex2();
+//		ae2.ex2();
+		ae2.ex3();
 		
 		
 	}
